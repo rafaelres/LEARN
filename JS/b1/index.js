ed70f419687500cd879e67f8d4ff800e1f4819be
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-04 08:28:10 -0300

    JavaAvancado

---
+++ JS/b1/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual
+< menor que
+<= menor ou igual
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)
+!== diferente estrito (valor e tipo)
+*/
+const num1 = 10
+const num2 = '10'
+const num3 = 10
+const num4 = '10'
+const comp1 = num1 === num2
+const comp2 = num3 !== num4
+console.log(comp1)
+console.log(comp2)
---
+++ JS/b2/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores Lógicos
+&& -> AND -> E - todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> E - apenas uma precisa ser verdadeira para retornar true
+! -> NOT -> NÃO - 
+*/
+// const expressaoAnd = true && true && false
+// const expressaoOr = true || false || false || false
+
+// const usuario = 'Rafael';
+// const senha = '123456';
+// const vaiLogar = usuario === 'Rafael' && senha === '123456'
+
+// console.log(!true);
