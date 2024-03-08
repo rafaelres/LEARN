709ca6a4aa1da3b40f982271ee517c3d13cd6934
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-08 16:28:14 -0300

    date

---
+++ JS/b8/index.js
@@ -0,0 +1,6 @@
+// const tresHoras = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000;
+// const data = new Date(0+umDia+tresHoras);
+// console.log(data.toString());
+const data = new Date(2019,3,20,15,14,27,500)
+console.log(data.toString())
