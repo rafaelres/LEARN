c2d4bf4f6d83dc06002b1fae3235e9627bbe966d
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-12 08:18:25 -0300

    Date

--- JS/b8/index.js
+++ JS/b8/index.js
@@ -2,5 +2,32 @@
 // const umDia = 60 * 60 * 24 * 1000;
 // const data = new Date(0+umDia+tresHoras);
 // console.log(data.toString());
-const data = new Date(2019,3,20,15,14,27,500)
-console.log(data.toString())
+// const data = new Date(2019,3,20,15,14,27,500) // a, m, d, M, s, ms,
+// const data = new Date('2019-04-20 23:15:59')
+// console.log('Dia', data.getDate())
+// console.log('Mês', data.getMonth() + 1)
+// console.log('Ano', data.getFullYear())
+// console.log('Hora', data.getHours())
+// console.log('Min', data.getMinutes())
+// console.log('Seg', data.getSeconds())
+// console.log('Dia semana', data.getDay()) // 0 - Domingo, 6- Sabado
+// console.log(data.toString())
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`
+}
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
+const data = new Date()
+const dataBrasil = formataData(data)
+console.log(dataBrasil)
