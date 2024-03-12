6aa6488a63e6c1924329e0b349e85be094ec9da5
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-12 09:02:11 -0300

    switch

--- JS/b9/index.js
+++ JS/b9/index.js
@@ -1,21 +1,58 @@
-const data = new Date()
-const diaSemana = data.getDay()
-let diaSemanaTexto;
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
 
-if (diaSemana === 0) {
-    diaSemanaTexto = 'Domingo'
-} else if (diaSemana === 1) {
-    diaSemanaTexto = 'Segunda'
-} else if (diaSemana === 2) {
-    diaSemanaTexto = 'Terça'
-} else if (diaSemana === 3) {
-    diaSemanaTexto = 'Quarta'
-} else if (diaSemana === 4) {
-    diaSemanaTexto = 'Quinta'
-} else if (diaSemana === 5) {
-    diaSemanaTexto = 'Sexta'
-} else if (diaSemana === 6) {
-    diaSemanaTexto = 'Sábado'
+ switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        return diaSemanaTexto;
+    case 1:
+        diaSemanaTexto = 'Segunda'
+        return diaSemanaTexto;
+    case 2:
+        diaSemanaTexto = 'Terça'
+        return diaSemanaTexto;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        return diaSemanaTexto;
+    case 4:
+        diaSemanaTexto = 'Quinta'
+        return diaSemanaTexto;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        return diaSemanaTexto;
+    case 6:
+        diaSemanaTexto = 'Sábado';
+        return diaSemanaTexto;
+    default:
+        diaSemanaTexto = '';
+        return diaSemanaTexto;
+    }
 }
 
+const data = new Date()
+const diaSemana = data.getDay()
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana)
+
 console.log(diaSemana, diaSemanaTexto)
+
+
+
+// IF ELSE ->
+// if (diaSemana === 0) {
+//     diaSemanaTexto = 'Domingo'
+// } else if (diaSemana === 1) {
+//     diaSemanaTexto = 'Segunda'
+// } else if (diaSemana === 2) {
+//     diaSemanaTexto = 'Terça'
+// } else if (diaSemana === 3) {
+//     diaSemanaTexto = 'Quarta'
+// } else if (diaSemana === 4) {
+//     diaSemanaTexto = 'Quinta'
+// } else if (diaSemana === 5) {
+//     diaSemanaTexto = 'Sexta'
+// } else if (diaSemana === 6) {
+//     diaSemanaTexto = 'Sábado'
+// } else {
+//     diaSemanaTexto = '';
+// }
+
