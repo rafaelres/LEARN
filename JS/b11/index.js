e9e8982b82d0addba3f040f922ccf1b4f20f678c
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-18 10:49:23 -0300

    var & let aprofundado

---
+++ JS/b11/index.js
@@ -0,0 +1,28 @@
+//let tem escopo de bloco {}
+// Var só tem escopo de função
+
+// const verdadeira = true
+
+// let nome = 'Rafael'
+// var nome2 = 'Rafael'
+
+
+// if (verdadeira) {
+//     let nome = 'Otavio';
+//     var nome2 = 'Rogério';
+
+//     if (verdadeira) {
+//         var nome2 = 'Ronaldo'
+//         let nome = 'Outra coisa';
+//    }
+// }
+
+// console.log(nome, nome2)
+var nome = 'Rafael';
+
+function falaOi () {
+    
+    console.log(nome)
+}
+
+falaOi()
