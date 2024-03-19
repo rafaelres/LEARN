0d3af8f1e24dfcee1eb235ea821110011e2d68dc
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-19 10:49:29 -0300

    for classico

---
+++ JS/b14/index.js
@@ -0,0 +1,10 @@
+// for (let i = 0; i <= 10; i++) {
+//     const par = i % 2 === 0 ? 'par' : 'impar';
+//     console.log(i, par)
+// }
+
+const frutas = ['maça', 'pera', 'uva']
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Indice ${i}`, frutas[i])
+}
