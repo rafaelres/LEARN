70bee1b485526dec5228431a686dc43bcdff533c
rafaelres <rafaelsiqueirinha@gmail.com>
2024-03-22 10:14:53 -0300

    for in

---
+++ JS/b16/index.js
@@ -0,0 +1,23 @@
+// const frutas = ['maça', 'morango', 'melancia']
+const pessoa = {
+    nome: 'Rafael',
+    sobrenome: 'Estevam',
+    idade: 19
+}
+
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave])
+}
+
+
+// for (let index in frutas) {
+//     console.log(frutas[index])
+// }
+
+// for (let i in frutas) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i])
+// }
