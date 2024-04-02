766a13103be5d1238c7554a09217f5e0f70414b1
rafaelres <rafaelsiqueirinha@gmail.com>
2024-04-02 09:25:36 -0300

    ex1 logica

---
+++ JS/b21/index.js
@@ -0,0 +1,13 @@
+function max(x, y){
+    return x> y ? x :y
+}
+
+const max2 = (x, y) => {
+    return x > y ? x :y
+}
+
+const max3 = (x, y) => x > y ? x : y;
+
+console.log(max(10, 2))
+console.log(max2(10, 20))
+console.log(max3(10, 21))
