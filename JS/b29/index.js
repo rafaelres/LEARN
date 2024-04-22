ef63cbc468e97cd38573eaa8a279619b7fe569c9
rafaelres <rafaelsiqueirinha@gmail.com>
2024-04-22 08:57:39 -0300

    tipos de funcao

---
+++ JS/b29/index.js
@@ -0,0 +1,31 @@
+// Declaração de função (Function hoisting)
+falaOi()
+function falaOi() {
+    console.log('Oie')
+}
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function() {
+    console.log('Sou um dado.')
+}
+
+function executaFuncao(funcao){
+    console.log('Vou executar sua função abaixo:');
+    funcao()
+}
+executaFuncao(souUmDado)
+
+// Arrow function
+const funcaoArrow = () => {
+    console.log('Sou uma arrow function')
+}
+funcaoArrow()
+
+// Dentro de um objeto
+const obj = {
+    falar: function(){
+        console.log('Estou falando...')
+    }
+}
+obj.falar()
