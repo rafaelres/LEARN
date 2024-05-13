583a2c986a675b73fcbef9bf53afe3eddecf5107
rafaelres <rafaelsiqueirinha@gmail.com>
2024-05-13 14:28:36 -0300

    Reorganização de Pastas :spark:

---
+++ JS/Arrays-avançado/58/index.js
@@ -0,0 +1,10 @@
+// const nomes = new Array('rafael', 'natan', 'gabriela')
+const nomes = ['rafael', 'gabriela', 'natan', 'danilo', 'sergio'];
+// nomes[2] = 'pedro';
+// delete nomes[2];
+// const removido = nomes.pop()
+// const removido2 = nomes.shift()
+// nomes.unshift('danilo');
+// nomes.unshift('sergio');
+const novo = nomes.slice(1,-1)
+console.log(novo);
