f36e57451035d86a8994b93c6de731600ff3746d
rafaelres <rafaelsiqueirinha@gmail.com>
2024-05-13 14:52:26 -0300

    Revisando Arrays

--- JS/Arrays-avançado/58/index.js
+++ JS/Arrays-avançado/58/index.js
@@ -1,10 +1,16 @@
-// const nomes = new Array('rafael', 'natan', 'gabriela')
+const nomes = new Array('rafael', 'natan', 'gabriela')
 const nomes = ['rafael', 'gabriela', 'natan', 'danilo', 'sergio'];
-// nomes[2] = 'pedro';
-// delete nomes[2];
-// const removido = nomes.pop()
-// const removido2 = nomes.shift()
-// nomes.unshift('danilo');
-// nomes.unshift('sergio');
+nomes[2] = 'pedro';
+delete nomes[2];
+const removido = nomes.pop()
+const removido2 = nomes.shift()
+nomes.unshift('danilo');
+nomes.unshift('sergio');
 const novo = nomes.slice(1,-1)
 console.log(novo);
+
+const nome = 'Rafael Estevam Siqueira';
+const nomes = nome.split(' ')
+const nome = ['Rafael', 'Estevam', 'Siqueira']
+const nomes = nome.join(' ')
+console.log(nomes)
